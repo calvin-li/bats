fe5a6d4fa9dd38dcd2f01a21ce36d736cfe32645
calvin-li <calli@ucdavis.edu>
2015-03-27 21:56:55 -0700

    stretched the 100 icon to fit box

--- app/src/main/res/raw/svg.py
+++ app/src/main/res/raw/svg.py
@@ -1,6 +1,6 @@
 from os import system
-svg = r'<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="WIDE" height="50px" viewBox="0 0 100 100" xml:space="preserve">   <text      x="50" y="80" fill="white" stroke="COLOR" stroke-width="5px" font-size="85px" text-anchor="middle" font-weight="bold" font-family="monospace">PERCENT</text></svg>'
-for i in range(60, 80):
+svg = r'<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="50px" height="50px" viewBox="-50 -80 100 100" xml:space="preserve">   <text      x="0" y="0" fill="white" stroke="COLOR" stroke-width="5px" font-size="85px" text-anchor="middle" font-weight="bold" font-family="monospace" transform="scale(XSCALE,1)">PERCENT</text></svg>'
+for i in range(0, 101):
 	color = "black"
 	if i < 20:
 		color = "red"
@@ -12,12 +12,12 @@ for i in range(60, 80):
 		color = "yellowgreen"
 	else:
 		color = "forestgreen"
-	width = "50px"
+	xscale = "1"
 	if i == 100:
-		width = "75px"
+		xscale = "0.6"
 	out = svg.replace("COLOR", color)
 	out = out.replace("PERCENT",str(i))
-	out = out.replace("WIDE", width)
+	out = out.replace("XSCALE", xscale)
 	file = "percent/" + str(i) + ".svg"
 	with open(file, "w") as f:
 		f.write(out)
